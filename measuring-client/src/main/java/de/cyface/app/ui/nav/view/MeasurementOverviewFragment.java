17f4e2db26586aaf0815ab8f256edaca5a1f2cfd
Armin <armin.schnabel@cyface.de>
2021-01-19 18:29:25 +0100

    [STAD-243] Migrate to Android 11 (#6)
    * Upgrade target-, compile SDK and build tools version 29->30
    
    * Request storage permission only until Android 9
    
    * Cleanup code
    
    * Update API URL for emulator scenario
    
    * Upgrade gradle from 5 to 6.8
    
    * Upgrade android gradle plugin to 4.1.1
    
    * Upgrade android kotlin plugin to 1.4
    
    * Upgrade all dependencies
    
    * Upgrade cyface-utils to 2.0.1
    
    * Upgrade cyface-energy-settings to 3.0.0
    
    * Update release section of the readme
    
    * Cleanup code

--- measuring-client/src/main/java/de/cyface/app/ui/nav/view/MeasurementOverviewFragment.java
+++ measuring-client/src/main/java/de/cyface/app/ui/nav/view/MeasurementOverviewFragment.java
@@ -141,7 +141,7 @@ public class MeasurementOverviewFragment extends Fragment {
     /**
      * A {@code View.OnClickListener} for the addButton action button used to add {@code Event}s.
      */
-    private View.OnClickListener addButtonClickListener = new View.OnClickListener() {
+    private final View.OnClickListener addButtonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             final GoogleMap googleMap = map.getGoogleMap();
@@ -256,10 +256,13 @@ public class MeasurementOverviewFragment extends Fragment {
         Validate.notNull(fragmentActivity);
         switch (item.getItemId()) {
             case R.id.export_menu_item:
-                if (ContextCompat.checkSelfPermission(fragmentActivity,
+                // Permission requirements: https://developer.android.com/training/data-storage
+                final boolean requiresWritePermission = Build.VERSION.SDK_INT < Build.VERSION_CODES.Q;
+                final boolean missingPermissions = ContextCompat.checkSelfPermission(fragmentActivity,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                         || ContextCompat.checkSelfPermission(fragmentActivity,
-                                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED;
+                if (requiresWritePermission && missingPermissions) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         requestPermissions(
                                 new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE,
