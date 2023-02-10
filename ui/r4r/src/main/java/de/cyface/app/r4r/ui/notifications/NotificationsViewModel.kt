0108099ad3cf543dc86bbef47000dd83763cde49
Armin <armin.schnabel@cyface.de>
2023-02-10 17:53:03 +0100

    [RFR-279] Add generated RFR ui

---
+++ ui/r4r/src/main/java/de/cyface/app/r4r/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package de.cyface.app.r4r.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
