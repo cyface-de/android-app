5161f9738f4e34cefe9a8996e30675d5dbbdb4ab
Armin <armin.schnabel@cyface.de>
2023-02-13 15:19:32 +0100

    [RFR-276] Adjust UI to be similar to iOS draft

--- ui/r4r/src/main/java/de/cyface/app/r4r/ui/statistics/StatisticsViewModel.kt
+++ ui/r4r/src/main/java/de/cyface/app/r4r/ui/statistics/StatisticsViewModel.kt
@@ -1,13 +1,13 @@
-package de.cyface.app.r4r.ui.sync
+package de.cyface.app.r4r.ui.statistics
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SyncViewModel : ViewModel() {
+class StatisticsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is sync Fragment"
+        value = "This is statistics Fragment"
     }
     val text: LiveData<String> = _text
 }
