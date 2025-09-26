f5c7a0f4e28b3a62315b00af50a3b7373eb06b19
Armin <armin.schnabel@cyface.de>
2025-09-26 08:17:09 +0200

    Assert hashes in SettingsViewModelTest

--- ui/digural/src/androidTest/kotlin/de/cyface/app/digural/capturing/settings/SettingsViewModelTest.kt
+++ ui/digural/src/androidTest/kotlin/de/cyface/app/digural/capturing/settings/SettingsViewModelTest.kt
@@ -18,6 +18,8 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.File
+import java.io.FileInputStream
+import java.security.MessageDigest
 
 @RunWith(AndroidJUnit4::class)
 class SettingsViewModelTest {
@@ -29,12 +31,11 @@ class SettingsViewModelTest {
     fun setup() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
 
-        // FIXME: This test may not be reproducing the issue as the test file is unrealistic small.
-
         // Erstellen einer temporären Testdatei mit bekanntem Inhalt
-        tempFile = File(context.cacheDir, "test_file.txt")
-        tempFile.writeText("Dies ist der Inhalt der Testdatei für den Kopiervorgang.")
+        //tempFile = File(context.cacheDir, "test_file.txt")
+        //tempFile.writeText("Dies ist der Inhalt der Testdatei für den Kopiervorgang.")
 
+        // Using a real model file (12 MB) also does not reproduce [LEIP-386]
         /*val testContext = InstrumentationRegistry.getInstrumentation().context
         val inputStream = testContext.resources.openRawResource(
             testContext.resources.getIdentifier("fixme_do_not_upload", "raw", testContext.packageName)
@@ -50,42 +51,47 @@ class SettingsViewModelTest {
 
     @After
     fun tearDown() {
-        // Aufräumen: Löschen der temporären Dateien
         tempFile.delete()
         File(context.getExternalFilesDir(null), "anon_model").delete()
     }
 
     @Test
     fun testSaveToFile() = runTest {
+        // Arrange
         val appSettings = AppSettings.getInstance(context)
         val cameraSettings = CameraSettings.getInstance(context)
         val customSettings = CustomSettings.getInstance(context)
-        // GIVEN: Ein Intent mit einem URI, der auf unsere Testdatei verweist
-        val intent = Intent().apply {
-            data = Uri.fromFile(tempFile)
-        }
+        val intent = Intent().apply { data = Uri.fromFile(tempFile) }
         val activityResult = ActivityResult(Activity.RESULT_OK, intent)
+        val viewModel = SettingsViewModel(appSettings, cameraSettings, customSettings)
 
-        val viewModel = SettingsViewModel(
-            appSettings,
-            cameraSettings,
-            customSettings,
-            )
-
+        // Act
         viewModel.modelFilePicked(activityResult, context)
 
-        // THEN: Prüfen, ob die Zieldatei existiert
+        // Assert
         val copiedFile = File(context.getExternalFilesDir(null), "anon_model")
         assertTrue(copiedFile.exists())
 
-        // THEN: Prüfen, ob der Inhalt der kopierten Datei korrekt ist
-        val copiedContent = copiedFile.readText()
-        assertEquals("Dies ist der Inhalt der Testdatei für den Kopiervorgang.", copiedContent)
+        // Compare Hashes of copied files content instead of byte size [LEIP-386].
+        // Unfortunately, this test did not reproduce [LEIP-386], even with a realistic model.
+        assertEquals("Byte size mismatch!", tempFile.length(), copiedFile.length())
+        assertEquals("MD5 mismatch!", tempFile.md5(), copiedFile.md5())
+        assertEquals("SHA-256 mismatch!", tempFile.sha256(), copiedFile.sha256())
+    }
 
-        // THEN: Byte-Größe vergleichen statt Inhalt
-        //val originalSize = tempFile.length()
-        //val copiedSize = copiedFile.length()
-        //Log.d("de.cyface", "$originalSize vs $copiedSize")
-        //assertEquals("Byte size mismatch!", originalSize, copiedSize)
+    private fun File.md5(): String = this.hash("MD5")
+    private fun File.sha256(): String = this.hash("SHA-256")
+
+    private fun File.hash(algorithm: String): String {
+        val buffer = ByteArray(8192)
+        val digest = MessageDigest.getInstance(algorithm)
+        FileInputStream(this).use { fis ->
+            var read = fis.read(buffer)
+            while (read > 0) {
+                digest.update(buffer, 0, read)
+                read = fis.read(buffer)
+            }
+        }
+        return digest.digest().joinToString("") { "%02x".format(it) }
     }
 }
